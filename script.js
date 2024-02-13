ff5fa179c84f74649d51a475b06fe6a371555957
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-13 10:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
-function convertToUppercase(){
+function convertToUpperCase(){
 	//extract the value by id
 	let input = document.getElementById('fname');
 	// get the value to write in the input 
 	     let value = input.value;
 	  //convert it into uppercase
-	    value = value.toUppercase();
+	    value = value.toUpperCase();
 	//update the value in the input feild
 	input.value = value;
 }
 // add eventlisteners to the input field to trigger the conversion when it loses focus
- document.getElementById('fname').addEventListener('blur',convertToUppercase);
+ document.getElementById('fname').addEventListener('blur',convertToUpperCase);
